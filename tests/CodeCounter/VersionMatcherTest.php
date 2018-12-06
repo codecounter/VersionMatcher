d0da6dfa4a6f60f484322d6301c66a5a31f3085d
codecounter <codecounter@126.com>
2018-12-06 21:41:30 +0800

    - test unit failure.

--- tests/CodeCounter/VersionMatcherTest.php
+++ tests/CodeCounter/VersionMatcherTest.php
@@ -15,7 +15,8 @@ class VersionMatcherTest extends TestCase {
         $this->assertTrue(\CodeCounter\VersionMatcher::test('1.1.0 < 1.2.0'));
 
         // 1.10.0 > 1.2.0
-        $this->assertTrue(\CodeCounter\VersionMatcher::test('1.10.0 > 1.2.0'));
+        // test for unit fail
+        $this->assertTrue(!\CodeCounter\VersionMatcher::test('1.10.0 > 1.2.0'));
 
     }
 
