bca0b61c4d80e1b0a22e0af07390ec75dec9134b
codecounter <codecounter@126.com>
2018-12-07 23:46:24 +0800

    - test line coverage < 80%.

--- tests/CodeCounter/VersionMatcherTest.php
+++ tests/CodeCounter/VersionMatcherTest.php
@@ -15,45 +15,45 @@ class VersionMatcherTest extends TestCase {
         $this->assertTrue(\CodeCounter\VersionMatcher::test('1.1.0 < 1.2.0'));
 
         // 1.10.0 > 1.2.0
-        $this->assertTrue(\CodeCounter\VersionMatcher::test('1.10.0 > 1.2.0'));
+        $this->assertTrue(!\CodeCounter\VersionMatcher::test('1.10.0 > 1.2.0'));
 
     }
-
-    public function testParam () {
-        // 2.0 > 1.0
-        $this->assertTrue(
-            \CodeCounter\VersionMatcher::test('ver > 1.0', array('ver' => '2.0'))
-        );
-
-        // 1.1.0 >= 1.1.0
-        $this->assertTrue(
-            \CodeCounter\VersionMatcher::test('ver >=1.0.0', array('ver' => '1.1.0'))
-        );
-    }
-
-    public function testLogic () {
-        // 1.0 < 1.5.0 < 2.0
-        $this->assertTrue(
-            \CodeCounter\VersionMatcher::test('ver > 1.0 && ver < 2.0', array('ver' => '1.5.0'))
-        );
-
-        // 1.0 < 1.5.0 < 2.0
-        $this->assertFalse(
-            \CodeCounter\VersionMatcher::test('ver < 1.0 || ver > 2.0', array('ver' => '1.5.0'))
-        );
-    }
-
-    public function testDefault () {
-        // 1.5.0 > 1.0.0
-        $this->assertTrue(
-            \CodeCounter\VersionMatcher::test('>1.0.0', array('ver' => '1.5.0'), array('defaultField' => 'ver'))
-        );
-
-        // 1.1.0 > 1.1.0
-        $this->assertTrue(
-            \CodeCounter\VersionMatcher::test('1.1.0', array('ver' => '1.1.0'), array('defaultField' => 'ver'))
-        );
-    }
+//
+//    public function testParam () {
+//        // 2.0 > 1.0
+//        $this->assertTrue(
+//            \CodeCounter\VersionMatcher::test('ver > 1.0', array('ver' => '2.0'))
+//        );
+//
+//        // 1.1.0 >= 1.1.0
+//        $this->assertTrue(
+//            \CodeCounter\VersionMatcher::test('ver >=1.0.0', array('ver' => '1.1.0'))
+//        );
+//    }
+//
+//    public function testLogic () {
+//        // 1.0 < 1.5.0 < 2.0
+//        $this->assertTrue(
+//            \CodeCounter\VersionMatcher::test('ver > 1.0 && ver < 2.0', array('ver' => '1.5.0'))
+//        );
+//
+//        // 1.0 < 1.5.0 < 2.0
+//        $this->assertFalse(
+//            \CodeCounter\VersionMatcher::test('ver < 1.0 || ver > 2.0', array('ver' => '1.5.0'))
+//        );
+//    }
+//
+//    public function testDefault () {
+//        // 1.5.0 > 1.0.0
+//        $this->assertTrue(
+//            \CodeCounter\VersionMatcher::test('>1.0.0', array('ver' => '1.5.0'), array('defaultField' => 'ver'))
+//        );
+//
+//        // 1.1.0 > 1.1.0
+//        $this->assertTrue(
+//            \CodeCounter\VersionMatcher::test('1.1.0', array('ver' => '1.1.0'), array('defaultField' => 'ver'))
+//        );
+//    }
 }
 
 ?>
